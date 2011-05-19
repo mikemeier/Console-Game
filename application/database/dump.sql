99c939de07a5a0064c8adacbffb0b1d9a089673d
mYkon <sk.mykon@gmail.com>
2011-05-19 16:46:40 +0200

    Updated SQL Dump

--- application/database/dump.sql
+++ application/database/dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 16. Mai 2011 um 17:30
+-- Erstellungszeit: 19. Mai 2011 um 16:46
 -- Server Version: 5.1.41
 -- PHP-Version: 5.3.1
 
@@ -16,15 +16,62 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `ip`
+--
+
+CREATE TABLE IF NOT EXISTS `ip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) DEFAULT NULL,
+  `value` varchar(15) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_A5E3B32D1D775834` (`value`),
+  KEY `IDX_A5E3B32DC54C8C93` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `ip_type`
+--
+
+CREATE TABLE IF NOT EXISTS `ip_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_9FB90BA61D775834` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_id` int(11) DEFAULT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(64) NOT NULL,
   `created` datetime NOT NULL,
-  `lastAction` datetime NOT NULL,
+  `last_action` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`),
+  UNIQUE KEY `UNIQ_8D93D649A03F5E9F` (`ip_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `ip`
+--
+ALTER TABLE `ip`
+  ADD CONSTRAINT `ip_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `ip_type` (`id`);
+
+--
+-- Constraints der Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`ip_id`) REFERENCES `ip` (`id`);
